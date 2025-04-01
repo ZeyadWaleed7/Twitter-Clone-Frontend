11cc9d3aeb03e3c81768455f4295495ca0ffebcd
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2025-04-01 12:29:00 +0200

    adding project code

---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,24 @@
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Rightbar from "../../components/Rightbar/Rightbar";
+import ExploreHead from "../../components/ExploreHead/ExploreHead";
+import Hashtags from "../../components/Hashtags/Hashtags";
+import "./Explore.css";
+
+const Explore = () => {
+  return (
+    <div className="layout">
+      <div className="content">
+        <Sidebar />
+        <main className="main-content">
+          <ExploreHead />
+          <Hashtags />
+        </main>
+        <Rightbar />
+      </div>
+    </div>
+  );
+};
+console.log("Explore Page Rendered");
+
+
+export default Explore;
