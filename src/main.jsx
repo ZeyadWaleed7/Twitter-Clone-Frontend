003acc028664f92b6fee55025dd37643bbc3099d
zeyad waleed <s-zeyad.nabil@zewailcity.edu.eg>
2025-04-01 13:02:31 +0200

    chaning styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import twitterLogo from './assets/twitter-logo.jpg'
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+// Set the favicon
+const favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
+favicon.type = 'image/jpeg';
+favicon.rel = 'icon';
+favicon.href = twitterLogo;
+if (!document.querySelector("link[rel='icon']")) {
+  document.head.appendChild(favicon);
+}
+
+// Set the title
+document.title = 'Twitter Clone';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
